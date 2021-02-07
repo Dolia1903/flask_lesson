d47ea13ac1b9e459a623e561a1e23eb5ec7a2454
Vladimir <v.dolidze93@gmail.com>
2021-02-07 02:01:55 +0200

    Added files for Django_DZ

---
+++ academy/models.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+
+
+class Lecturer(models.Model):
+    lecturer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+
+
+class Group(models.Model):
+    group_id = models.AutoField(primary_key=True)
+    course = models.CharField(max_length=50)
+    student = models.ManyToManyField(Student)
+    teacher = models.ForeignKey(Lecturer, on_delete=models.CASCADE, related_name='teacher_academy_set')
---
+++ academy/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Student, Lecturer, Group
+
+
+def get_index(request):
+    return HttpResponse("Hello, World!")
+
+
+def get_students(request):
+    students = Student.objects.all().order_by('first_name')
+    return render(request, 'academy/get_students.html', {'students': students})
+
+
+def get_lecturers(request):
+    lecturers = Lecturer.objects.all().order_by('last_name')
+    return render(request, 'academy/get_lecturers.html', {'lecturers': lecturers})
+
+
+def get_groups(request):
+    groups = Group.objects.all().order_by('course')
+    return render(request, 'academy/get_groups.html', {'groups': groups})
