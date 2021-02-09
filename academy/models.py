e0796fcf9da4a5dbff13ff2376f1a780a0529c2d
Vladimir <v.dolidze93@gmail.com>
2021-02-09 05:49:18 +0200

    Made some travis fixes

--- academy/models.py
+++ academy/models.py
@@ -1,6 +1,6 @@
-from django.conf import settings
+# from django.conf import settings
 from django.db import models
-from django.utils import timezone
+# from django.utils import timezone
 
 
 class Student(models.Model):
@@ -21,4 +21,5 @@ class Group(models.Model):
     group_id = models.AutoField(primary_key=True)
     course = models.CharField(max_length=50)
     student = models.ManyToManyField(Student)
-    teacher = models.ForeignKey(Lecturer, on_delete=models.CASCADE, related_name='teacher_academy_set')
+    teacher = models.ForeignKey(Lecturer, on_delete=models.CASCADE,
+                                related_name='teacher_academy_set')
