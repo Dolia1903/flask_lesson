3b62fa8a83c828939848527827899acc04c2339c
Vladimir <v.dolidze93@gmail.com>
2021-02-18 06:13:35 +0200

    Added files for 8 DZ

--- academy/urls.py
+++ academy/urls.py
@@ -3,11 +3,29 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_index),
-    path('students', views.get_students),
-    path('lecturers', views.get_lecturers),
-    path('groups', views.get_groups),
-    path('students/new', views.create_student),
-    path('lecturers/new', views.create_lecturer),
-    path('groups/new', views.create_group)
+    path('', views.get_students),
+
+    path('students', views.get_students, name='get_students'),
+    path('students/<int:student_id>/', views.get_student, name='get_student'),
+    path('students/<int:student_id>/edit/', views.edit_student,
+         name='edit_student'),
+    path('students/new', views.create_student, name='create_student'),
+    path('students/<int:student_id>/delete/', views.delete_student,
+         name='delete_student'),
+
+    path('lecturers', views.get_lecturers, name='get_lecturers'),
+    path('lecturers/<int:lecturer_id>/', views.get_lecturer,
+         name='get_lecturer'),
+    path('lecturers/<int:lecturer_id>/edit/', views.edit_lecturer,
+         name='edit_lecturer'),
+    path('lecturers/new', views.create_lecturer, name='create_lecturer'),
+    path('lecturers/<int:lecturer_id>/delete/', views.delete_lecturer,
+         name='delete_lecturer'),
+
+    path('groups', views.get_groups, name='get_groups'),
+    path('groups/<int:group_id>/', views.get_group, name='get_group'),
+    path('groups/<int:group_id>/edit/', views.edit_group, name='edit_group'),
+    path('groups/new', views.create_group, name='create_group'),
+    path('groups/<int:group_id>/delete/', views.delete_group,
+         name='delete_group')
 ]
--- academy/views.py
+++ academy/views.py
@@ -1,18 +1,43 @@
 from django.http import HttpResponse
 # from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, get_list_or_404, \
+    redirect
 
 from .forms import GroupForm, LecturerForm, StudentForm
 from .models import Group, Lecturer, Student
 
 
-def get_index(request):
-    return HttpResponse("Hello, World!")
+# def get_index(request):
+#     return HttpResponse("Hello, World!")
 
 
 def get_students(request):
     students = Student.objects.all().order_by('first_name')
     return render(request, 'academy/get_students.html', {'students': students})
+
+
+def get_student(request, student_id):
+    student = get_object_or_404(Student, student_id=student_id)
+    return render(request, 'academy/get_student.html', {'student': student})
+
+
+def edit_student(request, student_id):
+    student = get_object_or_404(Student, student_id=student_id)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            student = form.save(commit=False)
+            student.save()
+            return redirect('get_student', student_id=student_id)
+
+    form = StudentForm(instance=student)
+    return render(request, 'academy/edit_student.html', {'form': form})
+
+
+def delete_student(request, student_id):
+    student = get_object_or_404(Student, student_id=student_id)
+    student.delete()
+    return redirect('get_students')
 
 
 def get_lecturers(request):
@@ -21,9 +46,66 @@ def get_lecturers(request):
                   {'lecturers': lecturers})
 
 
+def get_lecturer(request, lecturer_id):
+    lecturer = get_object_or_404(Lecturer, lecturer_id=lecturer_id)
+    return render(request, 'academy/get_lecturer.html', {'lecturer': lecturer})
+
+
+def edit_lecturer(request, lecturer_id):
+    lecturer = get_object_or_404(Lecturer, lecturer_id=lecturer_id)
+    if request.method == 'POST':
+        form = LecturerForm(request.POST, instance=lecturer)
+        if form.is_valid():
+            lecturer = form.save(commit=False)
+            lecturer.save()
+            return redirect('get_lecturer', lecturer_id=lecturer_id)
+
+    form = LecturerForm(instance=lecturer)
+    return render(request, 'academy/edit_lecturer.html', {'form': form})
+
+
+def delete_lecturer(request, lecturer_id):
+    lecturer = get_object_or_404(Lecturer, lecturer_id=lecturer_id)
+    lecturer.delete()
+    return redirect('get_lecturers')
+
+
 def get_groups(request):
     groups = Group.objects.all().order_by('course')
     return render(request, 'academy/get_groups.html', {'groups': groups})
+
+
+def get_group(request, group_id):
+    group = get_object_or_404(Group, group_id=group_id)
+    teacher = get_object_or_404(Lecturer, lecturer_id=group.teacher_id)
+    students = get_list_or_404(Student, group=group_id)
+
+    context = {
+        'group': group,
+        'students': students,
+        'teacher': teacher
+    }
+
+    return render(request, 'academy/get_group.html', context)
+
+
+def edit_group(request, group_id):
+    group = get_object_or_404(Group, group_id=group_id)
+    if request.method == 'POST':
+        form = GroupForm(request.POST, instance=group)
+        if form.is_valid():
+            group = form.save(commit=False)
+            group.save()
+            return redirect('get_group', group_id=group_id)
+
+    form = GroupForm(instance=group)
+    return render(request, 'academy/edit_group.html', {'form': form})
+
+
+def delete_group(request, group_id):
+    group = get_object_or_404(Group, group_id=group_id)
+    group.delete()
+    return redirect('get_groups')
 
 
 def create_student(request):
