bd5db0c49fb048dbeac905d7f1e6067332165d07
Vladimir <v.dolidze93@gmail.com>
2021-02-21 04:45:59 +0200

    Added files for Signals DZ

--- academy/urls.py
+++ academy/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('groups/<int:group_id>/edit/', views.edit_group, name='edit_group'),
     path('groups/new', views.create_group, name='create_group'),
     path('groups/<int:group_id>/delete/', views.delete_group,
-         name='delete_group')
+         name='delete_group'),
 ]
